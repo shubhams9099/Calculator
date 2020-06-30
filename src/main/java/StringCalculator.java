bfa8831534b034386a63ca308f1c22d9bc59abbe
ss <shahs9099@gmail.com>
2020-07-01 01:14:47 +0530

    Adding numbers seperated by commas or \n. Task-3

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,11 +1,13 @@
+import java.util.regex.*;
 public class StringCalculator {
     public int Add(String numbers){
         int sum=0;
         if(numbers.length() ==0)
             return sum;
-        String nums[]=numbers.split(",");
-        for (int i = 0; i < nums.length; i++) {
-            sum+=Integer.parseInt(nums[i]);
+        Pattern pattern =Pattern.compile("(\\d)*[^\n,](\\d)*");
+        Matcher matcher=pattern.matcher(numbers);
+        while(matcher.find()){
+            sum+= Integer.parseInt(matcher.group());
         }
         return sum;
     }
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -4,22 +4,15 @@ public class StringCalculatorTest {
     @Test
     void testNumbersByComma1() {
         StringCalculator sc=new StringCalculator();
-        int expected=0;
-        int actual=sc.Add("");
+        int expected=10;
+        int actual=sc.Add("1\n2,3\n4");
         assertEquals(expected,actual);
     }
     @Test
     void testNumbersByComma2() {
         StringCalculator sc=new StringCalculator();
-        int expected=6;
-        int actual=sc.Add("1,2,3");
-        assertEquals(expected,actual);
-    }
-    @Test
-    void testNumbersByComma3() {
-        StringCalculator sc=new StringCalculator();
-        int expected=11;
-        int actual=sc.Add("1,10");
+        int expected=58;
+        int actual=sc.Add("11\n,3,44");
         assertEquals(expected,actual);
     }
 }
